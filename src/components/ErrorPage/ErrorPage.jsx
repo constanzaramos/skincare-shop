3439041d9da9f03c78a5722743150643d44fd0d1
Constanza Ramos <c.ramosgamboa@gmail.com>
2025-07-08 10:19:54 -0400

    fix import

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './ErrorPage.css'
+import './ErrorPage.scss'
 const ErrorPage = () => {
   return (
     <div className="error-page">
